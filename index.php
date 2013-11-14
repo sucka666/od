5a4c329e5eebdcad4c031a8c69a2ad7602ab555a
Paul Chersulich <paul.chersulich@gmail.com>
2013-11-13 20:57:28 -0500

    test

--- index.php
+++ index.php
@@ -1,13 +1,5 @@
 <?php
 
-/**
- * @file
- * The PHP page that serves all page requests on a Drupal installation.
- *
- * All Drupal code is released under the GNU General Public License.
- * See COPYRIGHT.txt and LICENSE.txt files in the "core" directory.
- */
-
 require_once __DIR__ . '/core/vendor/autoload.php';
 require_once __DIR__ . '/core/includes/bootstrap.inc';
 
